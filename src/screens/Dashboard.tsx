37d3b78a47062311f09553d62715077ae23ea993
Matheus Eufrasio <matheuseufrasio2@gmail.com>
2023-05-04 17:47:27 -0300

    Add typescript

---
+++ src/screens/Dashboard.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import {
+  View,
+  Text
+} from 'react-native';
+
+interface Props {
+  title: string;
+}
+
+export function Dashboard({ title }: Props) {
+  return (
+    <View>
+      <Text>{title}</Text>
+    </View>
+  );
+}
